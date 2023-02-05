766f935b2f4f9daf5510426ea96e0380aed58b8a
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-02-05 13:14:02 +0530

    Functional again

---
+++ functional.py
@@ -0,0 +1,28 @@
+from functools import reduce
+
+my_list = [1,2,3]
+ur_list = [10,20,30]
+their = (1000,2000,3000)
+def multiplyby2(item):
+    return item * 2
+
+def odd_only(item):
+    return item %2 != 0
+
+def accumulator(acc,item):
+    return acc + item
+#Map
+print(list(map(multiplyby2,my_list)))
+print(my_list)
+
+# Filter 
+print(list(filter(odd_only,my_list)))
+print(my_list)
+
+#Zip
+print(list(zip(my_list,ur_list,their)))
+print(my_list)
+
+#Reduce
+print(reduce(accumulator, my_list, 0))
+print(my_list)
--- map().py
+++
@@ -1,6 +0,0 @@
-my_list = [1,2,3]
-def multiplyby2(item):
-    return item * 2
-
-print(list(map(multiplyby2,my_list)))
-print(my_list)
