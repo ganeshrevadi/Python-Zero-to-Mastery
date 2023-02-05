a8db1b24d7545974b8fd66e41d04f04917943bff
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-02-05 12:55:42 +0530

    Functional programming

--- Inheritence.py
+++ Inheritence.py
@@ -2,15 +2,31 @@ class User:
     def login(self):
         print("Logged IN")
     
+    
+
+class Wizard(User):
+    def __init__(self,name,power):
+        self.name = name 
+    
     def attack(self):
         print(f'attacking by {self.name}')
 
-class Wizard(User):
-    def __init__(self,name):
+
+class Archer(User):
+    def __init__(self, name , arrows):
         self.name = name 
+        self.arrows = arrows
+
+    def check(self):
+        print(f'There are {self.arrows}  arrows remaining')
+
+w1 = Wizard('Merlin',50)
+
+class HybridBorg(Wizard,Archer):
+    def __init__(self,name,power,arrows):
+        Archer.__init__(self, name, arrows)
+        Wizard.__init__(self, name, power)
 
 
-w1 = Wizard('Merlin')
-
-print(w1.login())
-print(w1.attack())
+hb1 = HybridBorg('borgie', 50, 100)
+print(hb1.login())
