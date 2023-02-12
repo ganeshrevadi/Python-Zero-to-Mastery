c4f838f52f7c4e3c2e2afc8b270d422308c6e5bc
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-02-12 22:20:42 +0530

    File I/O

--- filei/o.py
+++ filei/o.py
@@ -1,7 +1,8 @@
-my_file = open('test.txt')
-
-print(my_file.read())
-my_file.seek(0)
-print(my_file.read())
-
-my_file.close()
+try:
+    with open('test.txt',mode = 'r+') as my_file:
+        print(my_file.read())
+        my_file.seek(0)
+        print(my_file.read())
+except FileNotFoundError as err:
+        print("file does not exist")
+        raise err
