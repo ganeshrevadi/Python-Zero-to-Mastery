aa1d34194a5c5d0f1f2f4755b7a4985a64a005cb
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-02-25 11:32:30 +0530

    Passwords checker with sha1

--- Email/email_sender.py
+++ Email/email_sender.py
@@ -1,12 +1,16 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Ganesh'
 email['subject'] = 'testing smtb'
 email['to'] = 'ganeshrevadi16@gmail.com'
 
-email.set_content('Testing yoooo')
+email.set_content(html.substitute({'name' :'Tintin'}),'html')
 
 with smtplib.SMTP(host = 'smtp.gmail.com',port=587) as smtp:
     smtp.ehlo()
