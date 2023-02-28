e2d2d29d21ef216bdcf73a7b424b04f582778317
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-02-28 11:38:46 +0530

    Web

--- Server/Web_serv.py
+++ Server/Web_serv.py
@@ -2,9 +2,9 @@ from flask import Flask ,render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('about.html')
+@app.route('/<username>')
+def hello_world(username = None):
+    return render_template('about.html', name = username)
 
 @app.route('/blog')
 def blog():
